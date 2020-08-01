78d01c687cfdefc2960c10bc2a9d5f75c5ce4f41
Divyansh-coder <56934046+Divyansh-coder@users.noreply.github.com>
2020-08-01 20:09:05 +0530

    created area_of_circle.py

---
+++ area_of_circle.py
@@ -0,0 +1,8 @@
+#import math to use pi value
+import math
+
+#take radius as (real number)input from user
+radius = float(input("Enter the radius of the circle: "))
+
+#print area of circle
+print("The area of the circle with radius",radius,"is :",math.pi*radius**2)
